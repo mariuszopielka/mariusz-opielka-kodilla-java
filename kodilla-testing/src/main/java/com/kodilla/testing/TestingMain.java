e98b4d8e1ed0b2d1b52303cc3334d25cb8ecd8d9
Mariusz Opielka <mario.opiel@gmail.com>
2020-12-09 22:57:42 +0100

    6.2 First-testing-calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,19 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator myCalculator = new Calculator();
+
+        int sum = myCalculator.add(3, 3);
+        int sub = myCalculator.subtract(4, 3);
+
+        if(sum >=0 && sub >=0) {
+            System.out.println("Adding a+b is: " + sum);
+            System.out.println("Subtracting a-b is: " + sub);
+        } else {
+            System.out.println("Error : value below zero !!!");
+        }
     }
 }
